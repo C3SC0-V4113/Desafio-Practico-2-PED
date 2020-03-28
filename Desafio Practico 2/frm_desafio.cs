6c6ba1a3b4788409fa30cf6ddf3b492648498035
C3SC0-V4113 <C3SC0-V4113@users.noreply.github.com>
2020-03-28 11:48:13 -0600

    Creado Repositorio
    Agregada Solucion y Desafio vacio al repositorio

---
+++ Desafio Practico 2/frm_desafio.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Desafio_Practico_2
+{
+    public partial class frm_desafio : Form
+    {
+        public frm_desafio()
+        {
+            InitializeComponent();
+        }
+    }
+}
